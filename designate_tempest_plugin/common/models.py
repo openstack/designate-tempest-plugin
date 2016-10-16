4f173f37d616a7fbfe2e747208400565e4fe1648
gecong1973 <ge.cong@zte.com.cn>
2016-10-16 09:27:14 +0800

    Add __ne__ built-in function
    In Python 3 __ne__ by default delegates to __eq__ and inverts the
    result, but in Python 2 they urge you to define __ne__ when you
    define __eq__ for it to work properly [1].There are no implied
    relationships among the comparison operators. The truth of x==y
    does not imply that x!=y is false. Accordingly, when defining __eq__(),
    one should also define __ne__() so that the operators will behave as
    expected.
    [1]https://docs.python.org/2/reference/datamodel.html#object.__ne__
    
    Change-Id: I5abcaa9d74712df591499e8334f1d627b2d7d1ca

--- designate_tempest_plugin/common/models.py
+++ designate_tempest_plugin/common/models.py
@@ -31,6 +31,9 @@ class ZoneFile(object):
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     @classmethod
     def from_text(cls, text):
         """Return a ZoneFile from a string containing the zone file contents"""
@@ -63,6 +66,9 @@ class ZoneFileRecord(object):
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __hash__(self):
         return hash(tuple(sorted(self.__dict__.items())))
